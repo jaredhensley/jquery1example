32f40edd6ec06e007ec8985e20e0c0cd3bedd53f
Jared Hensley <jahensle85@me.com>
2016-09-11 12:49:56 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  
+  $(".our-header").delay(3000).text("sunday funday at thinkful");
+  
+  $("#btn1").on("click", function() {
+    
+    alert("button 1 clicked");
+  });
+  
+  $("#btn2").on("click", function() {
+    
+    alert("button 2 clicked");
+  });
+  
+  $("#btn3").on("click", function() {
+    
+    alert("button 3 clicked");
+  });
+  
+});
+
